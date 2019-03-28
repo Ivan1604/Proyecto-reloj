32c671fc7c0ccb827fc5b64436df34ad795a2a60
Ivan <47404084+Ivan1604@users.noreply.github.com>
2019-03-28 11:08:41 -0500

    proyecto reloj con label

---
+++ src/ec/edu/insuperior/controlador/controlador.java
@@ -0,0 +1,23 @@
+
+package ec.edu.insuperior.controlador;
+
+import ec.edu.insuperior.vista.Principal;
+/**
+ *
+ * @author PC-IVAN
+ */
+public class controlador {
+    
+    public static void main(String[] args) {
+        Principal a=new Principal();
+        a.setVisible(true);
+       
+        
+    }
+           
+ }
+        
+        
+        
+    
+
